a8b10283f133d20fa0cd7ae51370860bb7055c38
Arthur Fiolet <fioletarthur@gmail.com>
2024-12-13 10:02:48 +0100

    wokflow functionnal ?

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import NotYetImplemented from './NotYetImplemented/NotYetImplemented';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotYetImplemented />
+    <NotYetImplemented/>
   </React.StrictMode>
 );
