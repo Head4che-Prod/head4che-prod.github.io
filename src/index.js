c49578e2482b57a36f5cff5b2926895d362f411b
Arthur Fiolet <fioletarthur@gmail.com>
2024-12-14 18:32:14 +0100

    page 404 & dynamic routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import NotYetImplemented from './NotYetImplemented/NotYetImplemented';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NotYetImplemented from "./NotYetImplemented/NotYetImplemented";
+import InexistantPage from "./404/404";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <NotYetImplemented />
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<InexistantPage />} />
+        <Route path="/" element={<NotYetImplemented />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
