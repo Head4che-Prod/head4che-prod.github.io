d3a28a593bf17bc0855c514e4e31607adda370cc
Arthur Fiolet <fioletarthur@gmail.com>
2024-12-15 15:52:41 +0100

    images path

--- src/NotYetImplemented/NotYetImplemented.js
+++ src/NotYetImplemented/NotYetImplemented.js
@@ -5,7 +5,7 @@ class NotYetImplemented extends Component {
   render() {
     return (
       <div class="notImplemented">
-        <img id="logo" src="./assets/head4che_white.svg" alt="logo"></img>
+        <img id="logo" src="https://head4che.co/assets/head4che_white.svg" alt="logo"></img>
         <h1>
           Cette page est en construction. Revenez bientôt pour découvrir les
           avancées du projet !
