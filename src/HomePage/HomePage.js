b3cc68ea0affdf1e93bf59a1b9561daeb286b7c7
Kilso <95025809+Kilso80@users.noreply.github.com>
2025-04-16 12:42:05 +0200

    News added

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -13,18 +13,59 @@ class HomePage extends Component {
               <img alt="logo-subwayout" src="assets/subwayout.png" />
             </div>
             <div className="header-links">
-              {/* <a href="#news">News</a> */}
+              <a href="#news">News</a>
               <a href="#documents">Documents</a>
             </div>
           </nav>
         </header>
         <div className="download">
-          <div>
-            <img alt="logo-subwayout" src="./assets/subwayout.png" />
-            <button disabled>
-              <WinLogo height="2em" />
-              <span>Download Now</span>
-            </button>
+          <div className="beautifulBlueBox">
+            <img
+              className="logo-swo"
+              alt="logo-subwayout"
+              src="./assets/subwayout.png"
+            />
+            <div className="alldownloads">
+              <div className="mainDownload">
+                <button disabled>
+                  <WinLogo height="2em" />
+                  <span>Download Now</span>
+                </button>
+              </div>
+              <div className="instructionManual">
+                {/* <a target="_blank" href="/docs/instruction-manual.pdf"> */}
+                <img src="./assets/info.png" alt=""></img>
+                {/* </a> */}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="news">
+          <h1>Traffic News</h1>
+          <div className="row">
+            <div className="news-display first">
+              <img src="./assets/news_image.jpg" />
+              <p className="date">26.05.2025</p>
+              <p className="event">Final Defense</p>
+            </div>
+            <div className="news-display instagram">
+              <div className="instagram-link">
+                <a
+                  href="https://www.instagram.com/head4che.prod.co"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="svg-icon"
+                ></a>
+                <a
+                  href="https://www.instagram.com/head4che.prod.co"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="username"
+                >
+                  @head4che.prod.co
+                </a>
+              </div>
+            </div>
           </div>
         </div>
         <div id="documents">
@@ -36,22 +77,26 @@ class HomePage extends Component {
                 <h2 className="name">Technical report n°1</h2>
               </div>
               <p className="date">December 2024</p>
-              <a target="_blank" href="/docs/defense-1.pdf"><button>&gt;</button></a>
+              <a target="_blank" href="/docs/defense-1.pdf">
+                <button>&gt;</button>
+              </a>
             </div>
             <div className="document">
               <div className="row">
                 <PdfIcon />
                 <h2 className="name">Technical report n°2</h2>
               </div>
-              <p className="date">March 2024</p>
-              <a target="_blank" href="/docs/defense-2.pdf"><button>&gt;</button></a>
+              <p className="date">March 2025</p>
+              <a target="_blank" href="/docs/defense-2.pdf">
+                <button>&gt;</button>
+              </a>
             </div>
             <div className="document">
               <div className="row">
                 <PdfIcon />
                 <h2 className="name">Technical report n°3</h2>
               </div>
-              <p className="date">Upcoming</p>
+              <p className="date">May 2025</p>
               <button disabled>&gt;</button>
             </div>
           </div>
@@ -62,7 +107,9 @@ class HomePage extends Component {
                 <h2 className="name">Book of specifications</h2>
               </div>
               <p className="date">November 2024</p>
-              <a target="_blank" href="/docs/specifications.pdf"><button>&gt;</button></a>
+              <a target="_blank" href="/docs/specifications.pdf">
+                <button>&gt;</button>
+              </a>
             </div>
           </div>
           <div className="bg"></div>
