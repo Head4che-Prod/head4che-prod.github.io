5ff44a2cb47522e00299167de136e3c276b43ef4
Adam-Graziani <adam.graziani@epita.fr>
2025-05-24 09:10:45 +0200

    Revert "Merge branch 'main' of https://github.com/Head4che-Prod/head4che-prod.github.io"
    This reverts commit a9014d00f5b0cea9942dc489d7953119906d6ca4, reversing
    changes made to f958012ddeb7ce0e7a9347f823a56e8ef5fc22ee.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import NotYetImplemented from "./NotYetImplemented/NotYetImplemented";
 import InexistantPage from "./InexistantPage/InexistantPage";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import HomePage from "./HomePage/HomePage";
-import HeadachePage from "./HeadachePage/HeadachePage";
-import ProjectPage from "./TheProject/ProjectPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HeadachePage />} />
-        <Route path="/subway-out/" element={<HomePage />} />
-        <Route path="/subway-out/the-project/" element={<ProjectPage />} />
+        <Route path="/" element={<NotYetImplemented />} />
         <Route path="*" element={<InexistantPage />} /> 
       </Routes>
     </BrowserRouter>
